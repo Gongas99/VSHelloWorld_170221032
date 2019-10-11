334d440068d4e10ac51ededc155ba92abbe85405
Gongas99 <gongaspatrocinio@gmail.com>
2019-10-11 17:39:10 +0100

    added solution
    added solution

---
+++ VSHelloWorld_170221032/VSHelloWorld_170221032/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace VSHelloWorld_170221032
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("https://github.com/Gongas99/VSHelloWorld_170221032");
+            Console.WriteLine("New Text");
+        }
+    }
+}
